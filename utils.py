ea08ae6180e4ab512032cdfcb094cc252e06dd25
deniseklep <deniseklep@hotmail.com>
2018-06-12 23:57:19 +0200

    pipeline finished but there were gpu issues, full run takes 10 hrs; utils fixed

--- utils.py
+++ utils.py
@@ -13,8 +13,6 @@ from keras.engine import Input, Model
 from keras.layers import Conv3D, MaxPooling3D, Activation, Cropping3D, UpSampling3D
 from keras.optimizers import Adam
 
-K.set_image_data_format("channels_first")
-
 from keras.layers.merge import concatenate
 
 
@@ -50,93 +48,76 @@ class PatchExtractor:
 
 
 class BatchCreator:
-
-    def __init__(self, patch_extractor, dataset, patch_indices, batch_division):
+    
+    def __init__(self,patch_extractor,dataset,patch_indices,batch_division):
         self.patch_extractor = patch_extractor
         self.patch_size = self.patch_extractor.patch_size
-
+        
         self.img_list = dataset['image'].values
         self.lbl_list = dataset['fissuremask'].values
         self.msk_list = dataset['lungmask'].values
-
+        
         self.a_indices = dataset.index[dataset['label'] == "a"].tolist()
         self.b_indices = dataset.index[dataset['label'] == "b"].tolist()
         self.c_indices = dataset.index[dataset['label'] == "c"].tolist()
-
+        
         self.img_indices = self.a_indices + self.b_indices + self.c_indices
-
+        
         self.fc_indices = patch_indices[0]
         self.fi_indices = patch_indices[1]
-
+        
         self.batch_division = batch_division
-
+        
         self.examined_images = []
-
+        
     def create_batch(self, batch_size):
-
+        
         if len(self.examined_images) == len(self.a_indices + self.b_indices + self.c_indices):
             self.examined_images = []
-            self.img_indices = self.a_indices + self.b_indices + self.c_indices
-
+            
         img_index = self.pickImage()
-
+        
         x_data, y_data, fissure_data = self.initializeOutputArrays(batch_size)
-
+        
         fc_slices_dict = self.fc_indices[img_index]
         fi_slices_dict = self.fi_indices[img_index]
-
+        
         img_array, lbl_array, msk_array = self.img2array(img_index)
 
-        (fc_nr, fi_nr) = self.batch_division
-        b_nr = batch_size - (fc_nr + fi_nr)
+        # get the minima for z, y, x to check if patches are in boundary
+        minima = self.getMinima(self.patch_extractor.patch_size,img_array.shape)
 
-        if len(list(fi_slices_dict.keys())) == 0:
-            fc_nr = fc_nr + fi_nr
-            fi_nr = 0
+        # get list of potential fissure complete and incomplete coords and the number of patches to generate
+        # for fissure complete, incomplete and background
+        fc_coords, fi_coords, fc_nr, fi_nr, b_nr = self.checkEmpty(batch_size,minima,fc_slices_dict,fi_slices_dict)
 
-        fc_grid, fc_grid_size = self.fissureGrid(fc_slices_dict)
-        fi_grid, fi_grid_size = self.fissureGrid(fi_slices_dict)
-        b_grid, b_grid_dict, b_grid_size = self.backgroundGrid(img_array.shape, int(b_nr / 4))
-
-        background_counter = 0
-        background_index = 0
-
+        # get list of potential background coords
+        b_coords = self.getBackground(msk_array,minima)
+        
         for i in range(batch_size):
             if i < fc_nr:
-                z = fc_grid[i % fc_grid_size]
-                (z, y, x) = self.getCoordinates(fc_slices_dict, z, img_array)
-                x_data[i] = self.patch_extractor.get_patch(img_array, (z, y, x), False)
-                y_data[i, 0, 0, 0, 2] = 1
-                fissure_data[i] = self.patch_extractor.get_patch(lbl_array, (z, y, x), True)
+                (z,y,x) = random.choice(fc_coords)
+                x_data[i] = self.patch_extractor.get_patch(img_array,(z,y,x),False)
+                y_data[i,0,0,0,2] = 1
+                fissure_data[i] = self.patch_extractor.get_patch(lbl_array,(z,y,x),True)
             elif ((i >= fc_nr) and (i < (fc_nr + fi_nr))):
-                z = fi_grid[i % fi_grid_size]
-                (z, y, x) = self.getCoordinates(fi_slices_dict, z, img_array)
-                x_data[i] = self.patch_extractor.get_patch(img_array, (z, y, x), False)
-                y_data[i, 0, 0, 0, 1] = 1
-                fissure_data[i] = self.patch_extractor.get_patch(lbl_array, (z, y, x), True)
+                (z,y,x) = random.choice(fi_coords)
+                x_data[i] = self.patch_extractor.get_patch(img_array,(z,y,x),False)
+                y_data[i,0,0,0,1] = 1
+                fissure_data[i] = self.patch_extractor.get_patch(lbl_array,(z,y,x),True)
             else:
-                if background_counter == 4:
-                    background_index += 1
-                    background_counter = 0
-                z = b_grid[background_index]
-                grid = b_grid_dict[z][background_counter]
-                (z, y, x) = self.getBackground(grid, msk_array, z)
-                x_data[i] = self.patch_extractor.get_patch(img_array, (z, y, x), False)
-                y_data[i, 0, 0, 0, 0] = 1
-                fissure_data[i] = self.patch_extractor.get_patch(lbl_array, (z, y, x), True)
-                background_counter += 1
-
-        self.examined_images.append(img_index)
-
-        return x_data, y_data
-
+                (z,y,x) = random.choice(b_coords)
+                x_data[i] = self.patch_extractor.get_patch(img_array,(z,y,x),False)
+                y_data[i,0,0,0,0] = 1
+                fissure_data[i] = self.patch_extractor.get_patch(lbl_array,(z,y,x),True)
+        
+        return x_data, fissure_data
+        
     def pickImage(self):
-        index = np.random.randint(0, len(self.img_indices) - 1)
-        img_index = self.img_indices[index]
+        img_index = self.img_indices[len(self.examined_images)]
         self.examined_images.append(img_index)
-        self.img_indices = np.delete(self.img_indices, index)
         return img_index
-
+    
     def initializeOutputArrays(self, batch_size):
         # patch array
         x_data = np.zeros((batch_size, *self.patch_extractor.patch_size,1))
@@ -144,103 +125,101 @@ class BatchCreator:
         y_data = np.zeros((batch_size, 1, 1, 1, 3))
         # fissure mask patch array
         fissure_data = np.zeros((batch_size, *self.patch_extractor.output_shape,1))
-
+        
         return x_data, y_data, fissure_data
-
+    
     def img2array(self, img_index):
         # compute numpy array from image
         img_path = self.img_list[img_index]
         img_array = readImg(img_path)
-
+        
         # compute numpy array from fissure mask
         lbl_path = self.lbl_list[img_index]
         lbl_array = readImg(lbl_path)
-
+        
         # compute numpy array from lung mask
         msk_path = self.msk_list[img_index]
         msk_array = readImg(msk_path)
         return img_array, lbl_array, msk_array
-
-    def fissureGrid(self, slicesDict):
-        z_size, _, _ = self.patch_extractor.patch_size
-        slices = sorted(list(slicesDict.keys()))
-        z_grid = list(self.chunks(slices, int(z_size * 1.5)))
-        z_medians = [int(np.median(chunk)) for chunk in z_grid]
-        grid_size = len(z_medians)
-        return z_medians, grid_size
-
-    def backgroundGrid(self, img_shape, b_nr):
-        z_max, y_max, x_max = img_shape
+    
+    def getMinima(self,patch_size,img_shape):
         z_size, y_size, x_size = self.patch_extractor.patch_size
-        slices = list(range(z_max))
-        z_grid = list(self.chunks(slices, int(len(slices) / b_nr)))
-        z_medians = [int(np.median(chunk)) for chunk in z_grid]
-        grid_size = len(z_medians)
-        z_grid_dict = {}
-        for z_median in z_medians:
-            grid1 = (0 + math.ceil(y_size / 2), int(y_max / 2) - 1, 0 + math.ceil(x_size / 2), int(x_max / 2) - 1)
-            grid2 = (
-            0 + math.ceil(y_size / 2), int(y_max / 2) - 1, int(x_max / 2), (x_max - 1) - (math.floor(y_size / 2)))
-            grid3 = (
-            int(y_max / 2), (y_max - 1) - (math.floor(y_size / 2)), 0 + math.ceil(x_size / 2), int(x_max / 2) - 1)
-            grid4 = (int(y_max / 2), (y_max - 1) - (math.floor(y_size / 2)), int(x_max / 2),
-                     (x_max - 1) - (math.floor(y_size / 2)))
-            z_grid_dict[z_median] = (grid1, grid2, grid3, grid4)
-        return z_medians, z_grid_dict, grid_size
-
-    def chunks(self, l, n):
-        return [l[i:i + n] for i in range(0, len(l), n)]
-
-    def getCoordinates(self, slices_dict, z, img_array):
-        coordinates = slices_dict[z]
-        filtered_coordinates = [(y, x) for (y, x) in coordinates if self.inBoundary((z, y, x), img_array.shape)]
+        z_max, y_max, x_max = img_shape
+        z_minimum = round(0+(z_size/2))
+        z_maximum = round(z_max-(z_size/2))
+        y_minimum = round(0+(y_size/2))
+        y_maximum = round(y_max-(y_size/2))
+        x_minimum = round(0+(x_size/2))
+        x_maximum = round(x_max-(x_size/2))
+        minima = ((z_minimum,z_maximum), (y_minimum,y_maximum), (x_minimum, x_maximum))
+        return minima
+    
+    def filterCoords(self,minima,slices_dict):
+        (z_minimum,z_maximum), (y_minimum,y_maximum), (x_minimum,x_maximum) = minima
+        slices = slices_dict.keys()
+        filtered_slices = [z for z in slices if z > z_minimum and z < z_maximum]
+        filtered_coordinates = []
+        for z in filtered_slices:
+            coords = slices_dict[z]
+            for (y,x) in coords:
+                if (y > y_minimum) and (y < y_maximum):
+                    if (x > x_minimum) and (x < x_maximum):
+                        filtered_coordinates.append((z,y,x))
+        return filtered_coordinates
+    
+    def getBackground(self,msk_array,minima):
+        (z_minimum,z_maximum), (y_minimum,y_maximum), (x_minimum,x_maximum) = minima
+        filtered_coordinates = []
+        for z in range(z_minimum,z_maximum):
+            y_indices, x_indices = np.where(msk_array[z,y_minimum:y_maximum,x_minimum:x_maximum] == 3)
+            for i, y in enumerate(y_indices):
+                x = x_indices[i]
+                filtered_coordinates.append((z,y+y_minimum,x+x_minimum))
         if len(filtered_coordinates) == 0:
-            print("Error: no x,y coordinates found that are in boundary of the image taking the patch size in mind")
-        random_coords_index = np.random.choice(len(filtered_coordinates))
-        y, x = filtered_coordinates[random_coords_index]
-        return (z, y, x)
-
-    def getBackground(self, grid, msk_array, z):
-        (y_min, y_max, x_min, x_max) = grid
-        y_indices, x_indices = np.where(msk_array[z, :, :] == 3)
-        coords = self.getCoords(z, y_indices, x_indices, msk_array)
-        i = np.random.randint(len(coords) - 1)
-        (y, x) = coords[i]
-        return (z, y, x)
-
-    def getCoords(self, z, y_indices, x_indices, msk_array):
-        coords = []
-        for i, y in enumerate(y_indices):
-            x = x_indices[i]
-            coord = (y, x)
-            if self.inBoundary((z, y, x), msk_array.shape):
-                coords.append(coord)
-        return coords
-
-    def inBoundary(self, location, img_shape):
-        _, y_size, x_size = img_shape
-        _, y_patch, x_patch = self.patch_extractor.patch_size
-
-        y_min = math.ceil(0 + (y_patch / 2))
-        y_max = math.floor(y_size - (y_patch / 2))
-
-        x_min = math.ceil(0 + (x_patch / 2))
-        x_max = math.ceil(x_size - (x_patch / 2))
-
-        _, y, x = location
-
-        if (y <= y_max and y >= y_min) and (x <= x_max and x >= x_min):
-            return True
+            print("Error: could not find background patch for slice %s, with y_minimum set to %s, y_maximum set to %s, x_minimum set to %s and x_maximum set to %s"%(z,y_minimum,y_maximum,x_minimum,x_maximum))
+        return filtered_coordinates
+    
+    def checkEmpty(self,batch_size,minima,fc_slices_dict,fi_slices_dict):
+        fc_coords = self.filterCoords(minima,fc_slices_dict)
+        fi_coords = []
+        
+        (fc_nr,fi_nr) = self.batch_division
+        b_nr = batch_size-(fc_nr+fi_nr)
+        
+        # if no 'fissure complete' coords are found inside boundaries
+        if len(fc_coords) == 0:
+            # if there are at least some 'fissure incomplete' slices
+            if not len(list(fi_slices_dict.keys())) == 0:
+                # make sure to skip fissure complete generation
+                # by setting number of fissure complete patches to zero
+                fi_nr = fc_nr + fi_nr
+                fc_nr = 0
+                fi_coords = self.filterCoords(minima,fi_slices_dict)
+                # if there are no 'fissure incomplete' coords inside boundaries
+                # make sure to skip fissure incomplete generation
+                if len(fi_coords) == 0:
+                    fi_nr = 0
+            # else skip both fissure complete and incomplete generation
+            else:
+                fc_nr = fi_nr = 0
         else:
-            return False
-
-    def checkBackground(self, location, lbl_array, msk_array):
-        z, y, x = location
-        if (lbl_array[z, y, x] == 0) and (msk_array[z, y, x] == 3):
-            return True
-        else:
-            return False
-
+            # if there are no 'fissure incomplete' slices
+            if len(list(fi_slices_dict.keys())) == 0:
+                #skip fissure incomplete generation
+                # by setting number of fissure incomplete patches to zero
+                fc_nr = fc_nr + fi_nr
+                fi_nr = 0
+            # if there are slices with fissure incomplete parts
+            else:
+                # find coords that are in boundary
+                fi_coords = self.filterCoords(minima,fi_slices_dict)
+                # if none are found, skip fissure incomplete patch generation
+                if len(fi_coords) == 0:
+                    fc_nr = fc_nr + fi_nr
+                    fi_nr = 0
+                    
+        return fc_coords, fi_coords, fc_nr, fi_nr, b_nr
+            
     def get_generator(self, batch_size):
         '''returns a generator that will yield batches infinitely'''
         while True:
